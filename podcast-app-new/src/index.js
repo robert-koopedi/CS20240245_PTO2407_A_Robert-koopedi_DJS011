fc21887852d64f8afb129fcae86a6a8abd2f6f0c
robert koopedi <koopedi05@gmail.com>
2025-05-24 00:52:39 +0200

    wrapped the app with the Audio Player Provider

--- podcast-app-new/src/index.js
+++ podcast-app-new/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AudioPlayerProvider } from './context/AudioPlayerContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <AudioPlayerProvider>
     <App />
+    </AudioPlayerProvider>
   </React.StrictMode>
 );
 
